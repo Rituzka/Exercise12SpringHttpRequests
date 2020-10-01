0cad35ef9c2e81faa3fab5ee33162ed10bb3f013
Rita <rituzka@gmail.com>
2020-10-01 12:26:37 +0200

    project structure

---
+++ src/test/java/com/itAcademy/springrequests/SpringrequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itAcademy.springrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
